d506a54ffff4da4ba9cf425efff793c42e182695
dp3why <dp3why@gmail.com>
2023-05-30 19:48:14 -0700

    port update

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the TCP port on which the Flask app is running
+    port = int(os.environ.get("PORT", 5000))
+    print("Running on port:", port)
+
+    # Start the Flask app
+    app.run(host='0.0.0.0', port=port, debug=True)
